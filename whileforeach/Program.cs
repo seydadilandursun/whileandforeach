5c5a46ee8867102fa5f30d1d7486f50481155ac1
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-23 20:58:02 +0300

    whileandforeach odevi yapildi

---
+++ whileforeach/Program.cs
@@ -0,0 +1,26 @@
+﻿//While and For Each 
+//While 
+//1den baslayarak consoledan girilen sayiya kadar sayi dahil ortalama hesaplayip konsola yazdiran program
+Console.WriteLine("Lutfen bir sayi giriniz:");
+int sayi=int.Parse(Console.ReadLine());
+int sayac=1;
+int toplam=0;
+    while (sayac<=sayi)
+{
+    toplam+=sayac;
+    sayac ++;
+}
+Console.WriteLine(toplam/sayi);
+// a dan z ye kadar tum harfleri console yazdir
+char character='a';
+while (character <'z')
+{
+  Console.Write(character);//Write yanyana yazdirir
+  character++;
+}
+//for each 
+string[] arabalar={"BMW","Ford","Toyota","Nissan"};
+foreach (var araba in arabalar)
+{
+    Console.WriteLine(araba);
+}
